4383e3b7cb0f3c52f01d4de32ecb4e3786f2f2fd
demkom58 <demkom58@gmail.com>
2020-09-10 02:54:06 +0300

    Close application on window close

--- src/main/java/com/demkom58/jaslab2/view/MainGui.java
+++ src/main/java/com/demkom58/jaslab2/view/MainGui.java
@@ -1,17 +1,23 @@
 package com.demkom58.jaslab2.view;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import java.awt.Dimension;
 
 public class MainGui extends JFrame {
-    private final Dimension minSize = new Dimension(450, 400);
     private JPanel rootPanel;
 
     public MainGui() {
         setContentPane(rootPanel);
+
+        final Dimension minSize = new Dimension(450, 400);
         setMaximumSize(minSize);
         setSize(minSize);
+
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         setTitle("Laboratory Work №2");
     }
 
