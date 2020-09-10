872e44302e2c96f5377d933549e06ce13b54671a
demkom58 <demkom58@gmail.com>
2020-09-10 21:49:53 +0300

    Use system theme ui

--- src/main/java/com/demkom58/jaslab2/Main.java
+++ src/main/java/com/demkom58/jaslab2/Main.java
@@ -2,10 +2,17 @@ package com.demkom58.jaslab2;
 
 import com.demkom58.jaslab2.view.MainGui;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         EventQueue.invokeLater(() -> new MainGui().setVisible(true));
     }
 }
