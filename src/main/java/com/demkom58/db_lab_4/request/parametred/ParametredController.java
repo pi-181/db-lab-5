9a38cd4c3df6315e0af0f898b92806273dbde749
demkom58 <demkom58@gmail.com>
2020-11-27 22:22:07 +0200

    ParametredController done

--- src/main/java/com/demkom58/db_lab_4/request/parametred/ParametredController.java
+++ src/main/java/com/demkom58/db_lab_4/request/parametred/ParametredController.java
@@ -1,8 +1,15 @@
 package com.demkom58.db_lab_4.request.parametred;
 
 import com.demkom58.db_lab_4.DatabaseManager;
+import com.demkom58.db_lab_4.request.paramless.RequestTableAdapter;
+import com.demkom58.db_lab_4.request.paramless.TableAdapter;
+import com.demkom58.db_lab_4.util.SwingUtil;
+import org.intellij.lang.annotations.Language;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.sql.PreparedStatement;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 
 public class ParametredController {
@@ -12,25 +19,70 @@ public class ParametredController {
     private final DatabaseManager databaseManager;
     private final ExecutorService service;
 
-    private final JTextField parametredFromField;
-    private final JTextField parametredToField;
-    private final JButton parametredGoButton;
-    private final JTable parametredResultTable;
-    private final JLabel parametredStatusBar;
+    private final JTextField fromField;
+    private final JTextField toField;
+    private final JButton goButton;
+    private final JTable resultTable;
+    private final JLabel statusBar;
+
+    private final TableAdapter tableAdapter;
 
     public ParametredController(JTabbedPane tabbedPane, JPanel requestPanel, DatabaseManager databaseManager, ExecutorService service,
-                                JTextField parametredFromField, JTextField parametredToField, JButton parametredGoButton,
-                                JTable parametredResultTable, JLabel parametredStatusBar) {
+                                JTextField fromField, JTextField toField, JButton goButton, JTable resultTable, JLabel statusBar) {
         this.tabbedPane = tabbedPane;
         this.requestPanel = requestPanel;
 
         this.databaseManager = databaseManager;
         this.service = service;
 
-        this.parametredFromField = parametredFromField;
-        this.parametredToField = parametredToField;
-        this.parametredGoButton = parametredGoButton;
-        this.parametredResultTable = parametredResultTable;
-        this.parametredStatusBar = parametredStatusBar;
+        this.fromField = fromField;
+        this.toField = toField;
+        this.goButton = goButton;
+        this.resultTable = resultTable;
+        this.statusBar = statusBar;
+
+        this.tableAdapter = new RequestTableAdapter(databaseManager, this.resultTable);
+        goButton.addActionListener(this::handleGoButton);
+    }
+
+    private void handleGoButton(ActionEvent event) {
+        final Optional<String> from = Optional.of(fromField.getText().trim()).map(t -> t.isEmpty() ? null : t);
+        final Optional<String> to = Optional.of(toField.getText().trim()).map(t -> t.isEmpty() ? null : t);
+
+        resultTable.removeAll();
+        goButton.setEnabled(false);
+        statusBar.setText("Executing...");
+        service.execute(() -> {
+            try {
+                PreparedStatement preparedStatement = databaseManager.getConnection()
+                        .prepareStatement(
+                                "SELECT * FROM social.remittance WHERE from_user = ? AND to_user = ?"
+                        );
+
+                from.ifPresent(s -> sneaky(() -> preparedStatement.setInt(1, Integer.parseInt(s))));
+                to.ifPresent(s -> sneaky(() -> preparedStatement.setInt(2, Integer.parseInt(s))));
+
+                resultTable.setModel(SwingUtil.buildTableModel(preparedStatement.executeQuery()));
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getLocalizedMessage(),
+                        "Error", JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
+            } finally {
+                goButton.setEnabled(true);
+                statusBar.setText("Idling...");
+            }
+        });
+    }
+
+    interface SneakyRunnable {
+        void run() throws Throwable;
+    }
+
+    public static void sneaky(SneakyRunnable e) throws RuntimeException {
+        try {
+            e.run();
+        } catch (Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
     }
 }
