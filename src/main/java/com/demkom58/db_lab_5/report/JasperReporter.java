caa9ba638bc7828cca3d2e7154367b0b7df283ac
demkom58 <demkom58@gmail.com>
2020-12-12 09:29:21 +0200

    Added listeners to menu items

--- src/main/java/com/demkom58/db_lab_5/report/JasperReporter.java
+++ src/main/java/com/demkom58/db_lab_5/report/JasperReporter.java
@@ -3,6 +3,7 @@ package com.demkom58.db_lab_5.report;
 import com.demkom58.db_lab_5.DatabaseManager;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.util.concurrent.ExecutorService;
 
 public class JasperReporter {
@@ -20,5 +21,22 @@ public class JasperReporter {
         this.simpleItem = simpleItem;
         this.nestedItem = nestedItem;
         this.statItem = statItem;
+
+        simpleItem.addActionListener(this::onSimpleReport);
+        nestedItem.addActionListener(this::onNestedReport);
+        statItem.addActionListener(this::onStatReport);
     }
+
+    protected void onSimpleReport(ActionEvent e) {
+        System.out.println("Generating simple report!");
+    }
+
+    protected void onNestedReport(ActionEvent e) {
+        System.out.println("Generating nested report!");
+    }
+
+    protected void onStatReport(ActionEvent e) {
+        System.out.println("Generating statistical report!");
+    }
+
 }
