cd7c743a377fa020a5785fb1623bca8cbcee4f5f
demkom58 <demkom58@gmail.com>
2020-12-14 02:18:26 +0200

    Simple & Nested reports

--- src/main/java/com/demkom58/db_lab_5/report/JasperReporter.java
+++ src/main/java/com/demkom58/db_lab_5/report/JasperReporter.java
@@ -1,9 +1,17 @@
 package com.demkom58.db_lab_5.report;
 
 import com.demkom58.db_lab_5.DatabaseManager;
+import com.demkom58.db_lab_5.util.ThrowableRunnable;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.swing.JRViewer;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 public class JasperReporter {
@@ -28,15 +36,51 @@ public class JasperReporter {
     }
 
     protected void onSimpleReport(ActionEvent e) {
-        System.out.println("Generating simple report!");
+        execute(() -> {
+            try (Connection c = databaseManager.getConnection()) {
+                showReport("Simple Report", "/jr/Simple.jrxml", new HashMap<>(), c);
+            }
+        });
     }
 
     protected void onNestedReport(ActionEvent e) {
-        System.out.println("Generating nested report!");
+        execute(() -> {
+            try (Connection c = databaseManager.getConnection()) {
+                showReport("Nested Report", "/jr/Nested.jrxml", new HashMap<String, Object>() {{
+                    put("groups_subreport", load("/jr/NestedSub_Group.jrxml"));
+                    put("subscribers_subreport", load("/jr/NestedSub_Subscribers.jrxml"));
+                }}, c);
+            }
+        });
     }
 
     protected void onStatReport(ActionEvent e) {
         System.out.println("Generating statistical report!");
+    }
+
+    private void showReport(String name, String template, Map<String, Object> parameters, Connection connection) throws IOException, JRException {
+        JasperPrint jasperPrint = JasperFillManager.fillReport(load(template), parameters, connection);
+        JFrame frame = new JFrame(name);
+        frame.getContentPane().add(new JRViewer(jasperPrint));
+        frame.setSize(850, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private void execute(ThrowableRunnable runnable) {
+        service.execute(() -> {
+            try {
+                runnable.run();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+                JOptionPane.showMessageDialog(null, throwable.getMessage(),
+                        "Error occurred", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
+
+    private JasperReport load(String report) throws JRException {
+        return JasperCompileManager.compileReport(JRXmlLoader.load((getClass().getResourceAsStream(report))));
     }
 
 }
